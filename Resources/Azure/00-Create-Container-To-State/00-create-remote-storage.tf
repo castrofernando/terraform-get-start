da3d51eb99785dce3515ff24f27732083b2c3059
Fernando Castro <castro.fernando@me.com>
2025-07-06 22:43:07 -0300

    Created a storage account for all tfstates. Also Included remote state to all examples(backend tag).

---
+++ Resources/Azure/00-Create-Container-To-State/00-create-remote-storage.tf
@@ -0,0 +1,59 @@
+#This storage will be used to save tfstate of others exercises:
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                            = "tfstate${random_string.resource_code.result}"
+  resource_group_name             = azurerm_resource_group.tfstate.name
+  location                        = azurerm_resource_group.tfstate.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+output "containerInfo" {
+  value = {
+    ResourceGroup : azurerm_resource_group.tfstate.name
+    StorageAccountName : azurerm_storage_account.tfstate.name
+    StorageContainer : azurerm_storage_container.tfstate.name
+  }
+
+}
--- Resources/Azure/02-Linux-VM/version.tf
+++ Resources/Azure/02-Linux-VM/version.tf
@@ -5,5 +5,11 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate0fe2z"
+    container_name       = "tfstate"
+    key                  = "02-Linux-VM/terraform.tfstate"
+  }
   required_version = ">= 1.1.0"
 }
